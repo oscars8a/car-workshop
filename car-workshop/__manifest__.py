4e92802d88b01a84e31de1a38bb2c7041495ff0b
sdi <oscar.soto.ochoa@gmail.com>
2017-12-18 11:00:05 +0100

    manifest

---
+++ car-workshop/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+{
+    'name': 'SDI Car Workshop',
+    'version': '10.0.1.0.0',
+    'summary': 'Complete Vehicle Workshop Operations & Reports',
+    'description': 'Vehicle workshop operations & Its reports',
+    'category': 'Industries',
+    'author':
+        'Óscar Soto, '
+        'Javier Izco, '
+        'Javier Garcia, '
+        'Sergio Lop, '
+        'Darío Lodeiros',
+    'website': 'https://github.com/oscars8a/car-workshop.git',
+    'license': 'GPL v3.0',
+    'depends': ['', ],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': [''],
+    }
+}
