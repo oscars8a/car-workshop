e613e4428c4d9bafb77ca3f70357bb7b9580b44f
Javier Izco <javier.izco@citylok.com>
2018-01-02 12:26:20 +0100

    Check warehouse tasks

--- car-workshop/models/inherit_project_task.py
+++ car-workshop/models/inherit_project_task.py
@@ -7,6 +7,12 @@ from odoo.tools import float_is_zero, float_compare, float_round
 class Task(models.Model):
     _inherit = 'project.task'
 
+    @api.model
+    def _default_warehouse_id(self):
+        company = self.env.user.company_id.id
+        warehouse_ids = self.env['stock.warehouse'].search([('company_id', '=', company)], limit=1)
+        return warehouse_ids
+
     @api.depends('material_line_ids.price_total')
     def _amount_all(self):
         for order in self:
@@ -20,6 +26,10 @@ class Task(models.Model):
     vehicle_id = fields.Many2one(comodel_name="fleet.vehicle", string="Vehicle", required=False, )
     material_line_ids = fields.One2many('project.task.material.line', 'material_line_id', string='Material Lines')
     amount_total = fields.Float(string='Total', store=True, readonly=True, compute='_amount_all')
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', string='Warehouse',
+        required=True, readonly=True,
+        default=_default_warehouse_id)
 
 class MaterialLine(models.Model):
     _name = 'project.task.material.line'
@@ -35,7 +45,7 @@ class MaterialLine(models.Model):
     product_id = fields.Many2one('product.template', string='Products')
     product_uom_qty = fields.Integer(string='Quantity')
     price_unit = fields.Float(string='Unit Price')
-    material_line_id = fields.Many2one('project.task', string="Task", ondelete='cascade')
+    material_line_id = fields.Many2one('project.task', string="Task Reference", ondelete='cascade')
     price_total = fields.Float(compute='_compute_amount', string='Total Amount', store=True)
 
     @api.onchange('product_id')
@@ -48,14 +58,14 @@ class MaterialLine(models.Model):
             return {}
         if self.product_id.type == 'product':
             precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-            product = self.product_id.with_context(warehouse=1)
+            product = self.product_id.with_context(warehouse=self.material_line_id.warehouse_id.id)
             product_qty = self._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
             if float_compare(product.virtual_available, product_qty, precision_digits=precision) == -1:
                 is_available = False
                 if not is_available:
                     message = _('You plan to sell %s %s but you only have %s %s available in %s warehouse.') % \
                               (self.product_uom_qty, self.product_id.name, product.virtual_available,
-                               product.uom_id.name, 'Empresa')
+                               product.uom_id.name, self.material_line_id.warehouse_id.name)
                     warning_mess = {
                         'title': _('Not enough inventory!'),
                         'message': message
