9f3d548d30d2f5c12eb1c3b2e884642adf84cbf7
sdi <oscar.soto.ochoa@gmail.com>
2018-02-08 11:43:55 +0100

    [FIX] repair count in Areas kanban

--- car-workshop/models/inherit_project_project.py
+++ car-workshop/models/inherit_project_project.py
@@ -4,5 +4,11 @@ from odoo import api, fields, models, _
 class Project(models.Model):
     _inherit = 'project.project'
 
-    car_work = fields.Boolean(string="It's car's work?")
+    def _compute_repair_count(self):
+        task_data = self.env['car_workshop.repair'].read_group([('project_id', 'in', self.ids), '|', ('stage_id.fold', '=', False), ('stage_id', '=', False)], ['project_id'], ['project_id'])
+        result = dict((data['project_id'][0], data['project_id_count']) for data in task_data)
+        for project in self:
+            project.task_count = result.get(project.id, 0)
 
+    car_work = fields.Boolean(string="It's car's work?")
+    repair_count = fields.Integer(compute='_compute_repair_count', string="Repairs")
