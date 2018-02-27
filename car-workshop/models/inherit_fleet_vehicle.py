ef5d759733c8cbd3690f31ae8f75507c54fb9aff
sdi <oscar.soto.ochoa@gmail.com>
2018-02-27 13:11:45 +0100

    [FIX] required customer in vehicle,[FIX] kanban repair info, put down driver in vehicle.

--- car-workshop/models/inherit_fleet_vehicle.py
+++ car-workshop/models/inherit_fleet_vehicle.py
@@ -4,5 +4,5 @@ from odoo import api, fields, models, _
 class Vehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
-    customer_id = fields.Many2one(comodel_name="res.partner", string="Customer", required=False, )
+    customer_id = fields.Many2one(comodel_name="res.partner", string="Customer", required=True, )
     image_client_vehicle = fields.Binary(string="Car image")
