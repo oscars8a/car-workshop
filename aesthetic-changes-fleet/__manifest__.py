dc10c1a597dbb68a2aa4274b8cbc455524c7e7de
sdi <oscar.soto.ochoa@gmail.com>
2018-03-01 12:44:16 +0100

    Aesthetic changes in fleet model

---
+++ aesthetic-changes-fleet/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SDI Aesthetic Changes Fleet',
+    'version': '11.0.1.0.0',
+    'summary': "Changes in the module Fleet to SDI.",
+    'description': 'Changes in the module Fleet for SDI.',
+    'category': 'Industries',
+    'author':
+        'Óscar Soto, '
+        'SDI Soluciones Informáticas',
+    'website': 'https://github.com/oscars8a/car-workshop.git',
+    'license': 'AGPL-3',
+    'depends': [
+        'fleet',
+    ],
+    'data': [
+        'views/inherit_fleet_vehicle_model.xml',
+    ],
+    'installable': True,
+}
