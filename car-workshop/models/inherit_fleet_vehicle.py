084985ba624784a13b61abf963337875763d5e8f
sdi <oscar.soto.ochoa@gmail.com>
2018-03-02 09:49:10 +0100

    repair smartbutton in vehicle form

--- car-workshop/models/inherit_fleet_vehicle.py
+++ car-workshop/models/inherit_fleet_vehicle.py
@@ -7,8 +7,17 @@ class Vehicle(models.Model):
     customer_id = fields.Many2one(comodel_name="res.partner", string="Customer", required=True, )
     image_client_vehicle = fields.Binary(string="Car image")
 
+    repairs_count = fields.Integer('Repairs', compute='_compute_repairs_count')
+
 
     @api.onchange('model_id')
     def _onchange_model_id(self):
         if self.model_id and not self.image_client_vehicle:
             self.image_client_vehicle = self.image_medium
+
+    def _compute_repairs_count(self):
+        repairs_obj = self.env['car_workshop.repair']
+        for vehicle in self:
+            vehicle.repairs_count = repairs_obj.search_count([
+                ('vehicle_id.id', '=', vehicle.id)
+            ])
