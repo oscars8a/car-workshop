aa235a9b4bd3c84a9b4ae2b034b084fdeb0993a8
sdi <oscar.soto.ochoa@gmail.com>
2018-03-12 12:17:42 +0100

    changes for default data for spanish version

---
+++ aesthetic-changes-spanish/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SDI Aesthetic Changes and Default data for Spanish version',
+    'version': '11.0.1.0.0',
+    'summary': "Changes in the module Fleet to SDI.",
+    'description': 'Changes in the module Fleet for SDI. Change the order in fleet model form view, '
+                   'first Make second Model name.'
+                    'And default data for Spanish version.',
+    'category': 'Industries',
+    'author':
+        'Óscar Soto, '
+        'SDI Soluciones Informáticas',
+    'website': 'https://github.com/oscars8a/car-workshop.git',
+    'license': 'AGPL-3',
+    'depends': [
+        'fleet',
+    ],
+    'data': [
+        'views/inherit_fleet_vehicle_model.xml',
+    ],
+    'installable': True,
+}
