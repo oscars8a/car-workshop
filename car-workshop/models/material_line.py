dc4b09687cded6eeec38a45bc1d9b77a935d2b18
sdi <oscar.soto.ochoa@gmail.com>
2018-03-20 13:02:58 +0100

    [ADD] consume_done method

--- car-workshop/models/material_line.py
+++ car-workshop/models/material_line.py
@@ -19,6 +19,7 @@ class MaterialLine(models.Model):
     location_id = fields.Many2one('stock.location', 'Source Location', index=True, required=True)
     location_dest_id = fields.Many2one('stock.location', 'Dest. Location', index=True, required=True)
     move_id = fields.Many2one('stock.move', 'Inventory Move', copy=False, readonly=True)
+
     product_id = fields.Many2one('product.product', 'Product', required=True)
     name = fields.Text(string='Description', required=True)
     product_uom_qty = fields.Float('Quantity', default=1.0, digits=dp.get_precision('  '),
@@ -28,11 +29,33 @@ class MaterialLine(models.Model):
 
     @api.multi
     def consume_done(self):
-        # depurador()
-        pass
+        Move = self.env['stock.move']
+        for record in self:
+            vals = {
+                'name': record.name,
+                'product_id': record.product_id.id,
+                'product_uom_qty': record.product_uom_qty,
+                'product_uom': record.product_uom.id,
+                'partner_id': record.repair_id.partner_id.id,
+                'location_id': record.location_id.id,
+                'location_dest_id': record.location_dest_id.id,
+                'move_line_ids': [(0, 0, {'product_id': record.product_id.id,
+                                          'lot_id': False,
+                                          'product_uom_qty': record.product_uom_qty,
+                                          'product_uom_id': record.product_uom.id,
+                                          'qty_done': record.product_uom_qty,
+                                          'package_id': False,
+                                          'result_package_id': False,
+                                          'location_id': record.location_id.id,
+                                          'location_dest_id': record.location_dest_id.id, })],
+                'car_work_repair_id': record.repair_id.id,
+                'origin': record.name,
+            }
+            move = Move.create(vals)
+            record.write({'move_id': move.id})
+            move._action_done()
 
 
-    # Obtener la descripción de product_id
     @api.onchange('repair_id', 'product_id', 'product_uom_qty')
     def onchange_product_id(self):
         partner = self.repair_id.partner_id
