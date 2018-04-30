4d8629a259c8e39c90ed17ec696ce48c0289e47b
Óscar Soto Ochoa <oscar.soto.ochoa@gmail.com>
2018-04-30 12:59:30 +0200

    [ADD] Color fecha deadline, campo description en factura (en pruebas)

--- car-workshop/models/inherit_account_invoice.py
+++ car-workshop/models/inherit_account_invoice.py
@@ -5,4 +5,20 @@ class AccountInvoice(models.Model):
     _name = "account.invoice"
     _inherit = "account.invoice"
 
+    def _compu_aux_descrip(self):
+        return self.description != ''
+
     description = fields.Html(string="Description")
+    aux_descrip = fields.Boolean(string="", compute=_compu_aux_descrip)
+
+class SaleOrder(models.Model):
+    _name = "sale.order"
+    _inherit = "sale.order"
+
+    @api.multi
+    def _prepare_invoice(self):
+        vals = super(SaleOrder, self)._prepare_invoice()
+        sale_id = self.env['sale.order'].search([('name','=',vals['origin'])])[0].id
+        obj = self.env['car_workshop.repair'].search([('sale_order_id',"=",sale_id)])[0]
+        vals['description'] = obj.description
+        return vals
